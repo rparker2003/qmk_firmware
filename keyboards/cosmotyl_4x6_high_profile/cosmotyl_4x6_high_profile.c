20c02607111452fd83f5771d5562d82f85468afc
Ryan Parker <rparker52003@gmail.com>
2024-11-13 01:25:31 -0800

    Initial commit of cosmotyl_4x6_high_profile, fixed dactyl
    Dactyl files would not compile and so I modified the json file and fixed
    it

---
+++ keyboards/cosmotyl_4x6_high_profile/cosmotyl_4x6_high_profile.c
@@ -0,0 +1,35 @@
+#include QMK_KEYBOARD_H
+#include "cosmotyl_4x6_high_profile.h"
+
+
+
+#ifdef POINTING_DEVICE_ENABLE
+uint8_t dpi = 0;
+void pointing_device_init_kb(void) {
+    dpi = dynamic_keymap_get_keycode(0,3,0);
+    pointing_device_set_cpi(dpi*100);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_F23:
+        if (record->event.pressed) {
+            dpi += 2;
+            if (dpi >= 82) dpi = 82;
+            dynamic_keymap_set_keycode(0,3,0,dpi);
+            pointing_device_set_cpi(dpi*100);
+        }
+        return false; // Skip all further processing of this key
+        case KC_F24:
+        if (record->event.pressed) {
+            dpi -= 2;
+            if (dpi <= 4) dpi = 4;
+            dynamic_keymap_set_keycode(0,3,0,dpi);
+            pointing_device_set_cpi(dpi*100);
+        }
+        return false; // Skip all further processing of this key
+        default:
+        return true; // Process all other keycodes normally
+        }
+    }
+#endif
